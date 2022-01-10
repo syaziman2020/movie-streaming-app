4dcc1db0e6f871d7f7a523af2a8d376af8e42815
*syaziman2020* <syazimanuray@gmail.com>
2022-01-10 21:14:33 +0700

    second commit

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -56,11 +56,14 @@ class SearchPage extends StatelessWidget {
               title: 'The Dark II',
               genre: 'Horror',
               starIconLast: Color(0xff363748),
+              shadow: Color.fromRGBO(22, 158, 159, 0.3),
             ),
             DisneyTile(
-                imageUrl: 'assets/movie6.png',
-                title: 'The Dark Knight',
-                genre: 'Heroes'),
+              imageUrl: 'assets/movie6.png',
+              title: 'The Dark Knight',
+              genre: 'Heroes',
+              shadow: Color.fromRGBO(22, 158, 159, 0.3),
+            ),
             DisneyTile(
               imageUrl: 'assets/movie7.png',
               title: 'The Dark Tower',
@@ -72,12 +75,50 @@ class SearchPage extends StatelessWidget {
       );
     }
 
+    Widget buttonSuggest() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 60,
+          left: 78,
+          right: 78,
+        ),
+        width: double.infinity,
+        height: 60,
+        decoration: BoxDecoration(
+          color: blueColor,
+          borderRadius: BorderRadius.circular(37),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(94, 56, 229, 0.2),
+              offset: Offset(0, 35),
+              blurRadius: 30,
+              spreadRadius: -10,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(37),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Suggest Movie',
+              style: whiteTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: primaryColor,
       body: ListView(
         children: [
           searchEngine(),
           searchItem(),
+          buttonSuggest(),
         ],
       ),
     );
